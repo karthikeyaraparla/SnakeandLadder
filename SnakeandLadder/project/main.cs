061fd996e31f289347b46dc09605702fd8dcd4ec
Raparla Karthikeya <karthikeyar1811@gmail.com>
2026-01-23 11:44:50 +0530

    Initialized player position

---
+++ SnakeandLadder/project/main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    class Random
+    {
+        static Random random = new Random();
+
+        Console.WriteLine("Welcome to Snake and Ladder Game");
+
+        int START_POSITION = 0;
+        int player_position = START_POSITION;
+
+        Console.WriteLine("Game started. Player is at position {player_position}.");
+
+
+    }
+}
